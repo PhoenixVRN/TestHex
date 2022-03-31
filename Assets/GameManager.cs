11c54251154fa2b2a046fae3fb4381e86ec46283
PhoenixVRN <projectfenixru@gmail.com>
2022-03-31 09:17:00 +0300

    31.03 1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameObject activHex;
+    public static bool isActivHex;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TouchHex.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class TouchHex : MonoBehaviour
+{
+    private bool _onActivUp; // нажат ли наш хекс.
+    private MeshRenderer _renderer;
+    void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        // проверяем рядом ли хекс который надо моменять местами.
+        if (GameManager.isActivHex && Vector3.Distance(GameManager.activHex.transform.position, transform.position) >
+                                   0.2
+                                   && Vector3.Distance(GameManager.activHex.transform.position, transform.position) <
+                                   1.2)
+        {
+            Debug.Log(GameManager.activHex.transform.position + " мой " + transform.position);
+            var a = transform.position;
+            var b = GameManager.activHex.transform.position;
+            GameManager.activHex.transform.position = a;
+            transform.position = b;
+            transform.position += new Vector3(0, -0.1f, 0);
+            GameManager.isActivHex = false;
+            GameManager.activHex.GetComponent<TouchHex>()._onActivUp = false;
+            GameManager.activHex = null;
+            _onActivUp = false;
+            return;
+        }
+        // логика если нажатый второй хекс дальше соседнего.
+        if (GameManager.isActivHex && !_onActivUp)
+        {
+            GameManager.activHex.transform.position += new Vector3(0, -0.1f, 0);
+            GameManager.activHex.GetComponent<TouchHex>()._onActivUp = false;
+            GameManager.activHex = gameObject;
+            _onActivUp = true;
+            transform.position += new Vector3(0, 0.1f, 0);
+            return;
+        }
+       
+        
+        // обработка начального нажатичя на хекс.
+        if (_onActivUp)
+        {
+            transform.Rotate(Vector3.up, 60);
+            transform.position += new Vector3(0, -0.1f, 0);
+            _onActivUp = false;
+            GameManager.isActivHex = false;
+            GameManager.activHex = null;
+        }
+        else
+        {
+            GameManager.activHex = gameObject;
+            GameManager.isActivHex = true;
+            transform.position += new Vector3(0, 0.1f, 0);
+            _onActivUp = true; 
+        }
+    }
+}
